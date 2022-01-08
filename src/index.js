86d37d85e10cd8bb99caf0a3f29d7218d7f05582
sariforest <iread1q84@gmail.com>
2022-01-08 17:12:30 +0900

    Try to use gql

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {
+    ApolloClient,
+    InMemoryCache,    
+    gql,
+} from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'localhost:4000',
+    cache: new InMemoryCache()    
+})
+
+client.query({
+    query: gql`
+        query hello: String!
+    `
+})
+.then(result => console.log(result));
 
 ReactDOM.render(
     <App />, 
